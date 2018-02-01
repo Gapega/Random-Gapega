3146738376116d79ae80b8bcc470241678ae86d7
gapega <gaponasega@gmail.com>
2018-02-01 08:20:18 -0500

    1/2

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Random-Gapega
+
+https://stackoverflow.com/questions/26069048/php-adding-a-guess-counter-to-a-random-number-game
---
+++ gamepage.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  </head>
+  <body><center>
+    <?php $numero=1; $maxtentativi=7; ?>
+    <h1>
+    Gioco dell'indovina numero    
+    </h1>
+    <p>
+      Tentativo n.<?php echo $numero;?><br> <b>Inserisci il numero</b>
+    </p>
+    <p>
+      <form action="">
+    <input type="input"/><br>
+        <input type="submit" value="Conferma" />
+</form>
+    </p>
+    </center>
+  </body>
+</html>
